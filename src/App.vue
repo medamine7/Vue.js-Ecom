e8dfc86909d12854ebb7762efbe87061ad13b467
Mohamed Amine Baladi <35896655+medamine7@users.noreply.github.com>
2018-05-31 04:08:28 +0000

    animate

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,29 @@
 <template>
   <div id="app" >
-    <v-app class="grey lighten-4">
-      <v-jumbotron  class="main-jumbo logo-container" height="30vw" color="white">
-        <v-container fill-height>
-          <v-layout align-center>
-            <v-flex>
-              <img id="logo" src="./assets/images/logo.png" alt="HASNAE KAFTAN" >                
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-jumbotron>
+    <v-app v-cloack class="grey lighten-4">
       <v-layout row wrap>
         <v-flex xs2 fill id="left-home-picture">
           <img src="./assets/images/henri-pham-348664-unsplash.jpg" alt="woman wearing a dress" >                
         </v-flex>
-        <v-flex xs10>
+        <v-flex xs12>
           <v-toolbar flat color="white">
             <v-toolbar-title hidden>KAFTAN</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="navbar">
-              <v-btn flat>Accueil</v-btn>
+              <v-btn to="/" flat>Accueil</v-btn>
               <v-btn flat>Collection</v-btn>
               <v-btn flat>Commander</v-btn>
-              <v-btn flat>Qui Somme nous?</v-btn>
+              <v-btn :to="{name:'aboutUs'}" flat>Qui Somme nous?</v-btn>
               <v-btn flat>Contact</v-btn>
             </v-toolbar-items>
           </v-toolbar>
           <router-view></router-view>
         </v-flex>
-        <v-container grid-list-xl pt-5> 
-          <h2 class="display-1 indicator mb-4">Produits Populaires</h2>
-          <v-layout row wrap justify-center>
-            <v-flex xl8 lg8 md10 sm10 xs12>
-              <v-layout wrap justify-center row>
-                <v-flex xl8 lg8 md8 sm12 xs12>
-                  <v-card flat hover class="wow fadeInUp" data-wow-delay="">
-                    <v-card-media src="/static/products/pietra-schwarzler-453095-unsplash.jpg" height="250px"></v-card-media>
-                    <v-card-title primary-title class="product-title"><strong>Kaftan Fasi </strong><v-spacer></v-spacer><span>1500MAD</span></v-card-title>
-                  </v-card>
-                </v-flex>
-                <v-flex v-for="i in 4" :key="i" xl3 lg4 md4 sm6 xs12>
-                  <v-card flat hover class="wow fadeInUp" data-wow-delay="">
-                    <v-card-media src="/static/products/pietra-schwarzler-453095-unsplash.jpg" height="250px"></v-card-media>
-                    <v-card-title primary-title class="product-title"><strong>Kaftan Fasi </strong><v-spacer></v-spacer><span>1500MAD</span></v-card-title>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-flex>
-          </v-layout>  
-          <v-btn large dark class="subheading wow fadeIn" depressed>Voir le reste</v-btn>
-        </v-container>
       </v-layout>
+      <!-- footer starts here -->
       <v-footer color="" class="pa-3 pt-5 footer wow fadeInUp" dark height="auto">
         <v-layout row wrap class="text-xs-left" justify-center>
-          <v-flex lg2 md2 sm4 xs12>
+          <v-flex lg2 md3 sm4 xs12>
             <h2 class="footer-heading pb-3">Collection</h2>
             <ul class="wow fadeInLeft">
               <li><a href="">Swiri</a></li>
@@ -62,20 +32,20 @@
               <li><a href="">Chamali</a></li>
             </ul>
           </v-flex>
-          <v-flex lg2 md2 sm4 xs12>
+          <v-flex lg2 md3 sm4 xs12>
             <h2 class="footer-heading pb-3">Commande</h2>
-            <ul class="wow fadeInLeft">
+            <ul class="wow fadeInTop">
               <li><a href="">Statut de commande</a></li>
             </ul>
           </v-flex>
-          <v-flex lg2 md2 sm4 xs12>
+          <v-flex lg2 md3 sm4 xs12>
             <h2 class="footer-heading pb-3">Qui somme nous?</h2>
-            <ul class="wow fadeInLeft">
-              <li><a href="">Hasnae Kaftan</a></li>
+            <ul class="wow fadeInRight">
+              <li><a href="">Hasna Kaftan</a></li>
             </ul>
           </v-flex>
           <v-flex xs12>
-            <div class="text-xs-center mt-3">&copy; Hasnae Kaftan {{ new Date().getFullYear() }} Tous droits réservées </div>
+            <div class="text-xs-center mt-3">&copy; Hasna Kaftan {{ new Date().getFullYear() }} Tous droits réservées </div>
           </v-flex>
         </v-layout>
       </v-footer>
@@ -113,6 +83,24 @@
   color: #2c3e50;
 }
 
+
+
+#left-home-picture{
+  height: 100vh;
+  width: auto;
+  overflow: hidden;
+  position: absolute;
+  z-index:1;
+
+  img{
+    margin: auto;
+    height: 135%;
+    width: auto;  
+    position: relative;
+    left: -86%;
+  }
+}
+
 .navbar{
   .btn{
     font-size: 13px;
@@ -120,63 +108,17 @@
   }  
 } 
 
-#logo{
-  width: 100%;
-}
-
-
-#left-home-picture{
-  height: 100vh;
-  width: auto;
-  overflow: hidden;
-
-  img{
-    margin: auto;
-    height: 135%;
-    position: absolute;
-    width: auto;  
-    position: relative;
-    left: -86%;
-  }
-}
-
-.main-jumbo{
-  width: 30vw;
-  position: absolute;
-  z-index: 10;
-  top: 10%;
-  left: 11%;
-}
-
-.product-container{
-  width:300px;
-  height:300px;
-  overflow: hidden;
-  margin: auto;
-  display:flex;
-  flex-direction: column;
-  justify-content: center;
-
-  img{
-    width: 100%;
-  }
-}
-
-.product-title{
-  font-family:karla;
-  font-size : 15px;
-}
-
-.indicator{
-  font-weight:bold;
-}
-
 .footer-heading{
     font-weight:bold;
     text-transform:capitalize;
 }
 
+[v-cloack]{
+  display:none;
+}
+
 .footer{
+  z-index:3;
   a{
     text-decoration:none;
     color: #afafaf;
---
+++ src/components/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <v-container text-xs-center fluid class="pa-0 index-container">
+      <v-jumbotron  class="main-jumbo logo-container " height="30vw" color="white">
+        <v-container fill-height>
+          <v-layout wrap align-center>
+            <v-flex >
+              <img id="logo" src="../assets/images/logo.png" alt="HASNAE KAFTAN" >                
+            </v-flex>
+            <v-flex class="text-xs-left">
+              <h2 class="title">Haute Couture Traditionnelle</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, laudantium.</p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+      <v-layout row wrap justify-end >
+        <v-flex xs8 id="right-home-picture">
+          <v-carousel hide-delimiters class="hero-carousel">
+            <v-carousel-item v-for="(image,index) in images" :key="index"
+            :src="image"
+            transition="fade"
+            reverse-transition="fade"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container grid-list-xl pt-5> 
+      <h2 class="display-1 indicator mb-4">Produits Populaires</h2>
+      <v-layout row wrap justify-center>
+        <v-flex xl8 lg8 md10 sm10 xs12>
+          <v-layout wrap justify-center row>
+            <v-flex xl8 lg8 md8 sm12 xs12>
+              <v-card flat hover class="wow fadeInUp" data-wow-delay="0">
+                <v-card-media src="/static/products/pietra-schwarzler-453095-unsplash.jpg" height="250px"></v-card-media>
+                <v-card-title primary-title class="product-title"><strong>Kaftan Fasi </strong><v-spacer></v-spacer><span>1500MAD</span></v-card-title>
+              </v-card>
+            </v-flex>
+            <v-flex v-for="i in 4" :key="i" xl3 lg4 md4 sm6 xs12>
+              <v-card flat hover class="wow fadeInUp" data-wow-delay="">
+                <v-card-media src="/static/products/pietra-schwarzler-453095-unsplash.jpg" height="250px"></v-card-media>
+                <v-card-title primary-title class="product-title"><strong>Kaftan Fasi </strong><v-spacer></v-spacer><span>1500MAD</span></v-card-title>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>  
+      <v-btn large dark class="subheading wow fadeIn" depressed>Voir le reste</v-btn>
+    </v-container>
+    <v-container fluid grid-list-xl pt-5 class="white"> 
+      <h2 class="display-1 indicator mb-4">Qui somme nous?</h2>
+      <v-layout row wrap justify-center>
+        <v-flex xl3 lg3 md3 sm12 xs12>
+          <img id="portrait" class="wow fadeInUp" src="/static/hasna.jpg" alt="">
+        </v-flex>
+        <v-flex class="text-xs-left" xl6 ls6 md6 sm12 xs12>
+          <p id="portrait-text" class="title wow fadeInRight  ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum natus saepe recusandae perferendis possimus cupiditate nam assumenda dolorem harum quidem ab voluptatum laudantium suscipit, quos dicta eligendi odio ipsam perspiciatis praesentium inventore quia dolores dignissimos aspernatur tempora. Facilis, commodi, quibusdam.</p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        images : [
+          '/static/naganath-chiluveru-345852-unsplash.jpg',
+          '/static/pete-bellis-310093-unsplash.jpg',
+          '/static/pete-bellis-189599-unsplash.jpg'
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+#logo{  
+  width: 100%;
+}
+
+#right-home-picture{
+  overflow: hidden;
+  height: 91.2vh;
+  img{
+    width:180%;
+  }
+}
+
+.fade{
+   &-enter-active, &-leave-active, &-leave-to{
+     transition: .3s ease-out;
+     position: absolute;
+     top: 0;
+     left: 0;
+   }
+
+    &-enter, &-leave, &-leave-to{
+      opacity: 0;
+    }
+}
+
+.hero-carousel{
+  height: 91vh;
+  box-shadow: none;
+}
+
+.index-container{
+  position: relative;
+}
+
+.main-jumbo{
+  width: 30vw;
+  position: absolute;
+  z-index: 2;
+  top: 10%;
+  left: 11%;
+
+  h2{
+    font-weight:bold;
+  }
+}
+
+.product-container{
+  width:300px;
+  height:300px;
+  overflow: hidden;
+  margin: auto;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+
+  img{
+    width: 100%;
+  }
+}
+
+.product-title{
+  font-family:karla;
+  font-size : 15px;
+}
+
+.indicator{
+  font-weight:bold;
+}
+
+#portrait{
+  width: auto;  
+  height: 250px;
+}
+
+#portrait-text{
+  // text-align:justify;
+}
+
+</style>
